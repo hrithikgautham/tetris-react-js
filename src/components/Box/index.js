0176458169cc366b4fbc821b6aa5bef1c449f68f
hrithik <htg2ana@gmail.com>
2019-08-15 21:21:08 +0530

    first commit

---
+++ src/components/Box/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.scss';
+
+function Box({ color }) {
+    return (
+        <div className="box" style={{backgroundColor: color}}>
+            
+        </div>
+    )
+}
+
+export default Box;
---
+++ src/components/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Box from "./Box";
+
+function MainComponent() {
+    return (
+        <div>
+            <Box/>
+        </div>
+    )
+}
+
+export default MainComponent;
